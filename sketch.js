daf28b70acb8a3368e5f7c11faf2bbdc622218f3
Neha2468 <81560725+Neha2468@users.noreply.github.com>
2021-08-19 20:21:50 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var canvas;
+var backgroundImage;
+var database, gameState;
+var form, player, playerCount;
+var allPlayers;
+var car1_img, car2_img, car1, car2, cars;
+var track;
+
+function preload() {
+  backgroundImage = loadImage("assets/background.png");
+  car1_img = loadImage("assets/car1.png");
+  car2_img = loadImage("assets/car2.png");
+  track = loadImage("assets/track.png");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw() {
+  background(backgroundImage);
+
+
+  if(playerCount === 2){
+    game.update();
+  }
+  if(gameState ===1){
+    game.play();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
